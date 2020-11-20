82b18bfb8a4737cbc8dcad33656d19d54ae74704
pkorenev <p.korenev@voroninstudio.eu>
2020-11-20 22:32:59 +0200

    add console.log variable from process.env

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+const STRIPE_PUBLIC_KEY = process.env.STRIPE_PUBLIC_KEY
+console.log(`STRIPE_PUBLIC_KEY: ${STRIPE_PUBLIC_KEY}`)
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
